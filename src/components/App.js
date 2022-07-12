f6290b00e1e121207c907e13705ebbc7cd9c4ffb
ghofran <gho0ofran.adel14@gmail.com>
2022-07-12 22:05:27 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -6,15 +6,20 @@ import itemData from "../data/items";
 function App() {
   const [items, setItems] = useState(itemData);
   const [isDarkMode, setIsDarkMode] = useState(false);
+ 
+  function onItemFormSubmit(newItem) {
+    setItems([...items, newItem]);
+  }
 
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
+  
 
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList items={items} onItemFormSubmit={onItemFormSubmit} />
     </div>
   );
 }
--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,35 @@
 import React from "react";
 import { v4 as uuid } from "uuid";
+import  { useState } from "react";
 
-function ItemForm(props) {
+function ItemForm({onItemFormSubmit}) {
+
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("Produce");
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleCategoryChange(event) {
+    setCategory(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    onItemFormSubmit({ name, category, id: uuid() });
+  }
+
   return (
-    <form className="NewItem">
+    <form  onSubmit={handleSubmit} className="NewItem">
       <label>
         Name:
-        <input type="text" name="name" />
+        <input onChange={handleNameChange} type="text" name="name" />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select onChange={handleCategoryChange} name="category">
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
